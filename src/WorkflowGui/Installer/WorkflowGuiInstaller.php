c0e725160d8d72325fdee0734fd489d733ce49ae
Joris Ros <joris.ros@gmail.com>
2021-05-24 22:55:55 +0200

    Add add/remove permission option to installer

--- src/WorkflowGui/Installer/WorkflowGuiInstaller.php
+++ src/WorkflowGui/Installer/WorkflowGuiInstaller.php
@@ -14,17 +14,38 @@
 
 namespace Youwe\Pimcore\WorkflowGui\Installer;
 
+use Pimcore\Db;
 use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
+use Pimcore\Model\User\Permission;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 final class WorkflowGuiInstaller extends SettingsStoreAwareInstaller
 {
+    const WORKFLOW_GUI = 'workflow_gui';
+
     public function install(): void
     {
+        $this->addPermissionToPanel();
         $this->markInstalled();
     }
 
     public function uninstall(): void
     {
+        $this->removePermissionFromPanel();
         $this->markUninstalled();
+    }
+
+    private function addPermissionToPanel(): void
+    {
+        $permissionDefinition = new Permission\Definition();
+        $permissionDefinition->setKey(self::WORKFLOW_GUI);
+        $permissionDefinition->save();
+    }
+
+    private function removePermissionFromPanel(): void
+    {
+        $state = Db::get()->prepare("DELETE FROM `users_permission_definitions` WHERE `key` = :key");
+        $state->bindValue("key", self::WORKFLOW_GUI);
+        $state->execute();
     }
 }
